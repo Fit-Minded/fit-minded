ae72c8697ad95e708e631349a5481f8dfa18ee58
Roy-Tessler <23tessler@gmail.com>
2020-02-02 13:24:59 -0500

    decision page

--- src/store/user.js
+++ src/store/user.js
@@ -1,57 +1,58 @@
-import axios from 'axios'
-import history from '../history'
+import axios from "axios";
+import history from "../history";
 
-const GET_USER = 'GET_USER'
-const REMOVE_USER = 'REMOVE_USER'
+const GET_USER = "GET_USER";
+const REMOVE_USER = "REMOVE_USER";
+const GET_CURRENT_USER = "GET_CURRENT_USER";
 
-const defaultUser = {}
+const defaultUser = {};
 
-const getUser = user => ({ type: GET_USER, user })
-const removeUser = () => ({ type: REMOVE_USER })
+const getUser = user => ({ type: GET_USER, user });
+const removeUser = () => ({ type: REMOVE_USER });
 
 export const me = () => async dispatch => {
   try {
-    const res = await axios.get('/auth/me')
-    dispatch(getUser(res.data || defaultUser))
+    const res = await axios.get("/auth/me");
+    dispatch(getUser(res.data || defaultUser));
   } catch (err) {
-    console.error(err)
+    console.error(err);
   }
-}
+};
 
 export const auth = (email, password, method) => async dispatch => {
-  let res
+  let res;
   try {
-    res = await axios.post(`/auth/${method}`, { email, password })
-    history.push('/home')
+    res = await axios.post(`/auth/${method}`, { email, password });
+    history.push("/home");
   } catch (authError) {
-    return dispatch(getUser({ error: authError }))
+    return dispatch(getUser({ error: authError }));
   }
 
   try {
-    await dispatch(getUser(res.data))
-    history.push('/home')
+    await dispatch(getUser(res.data));
+    history.push("/home");
   } catch (dispatchOrHistoryErr) {
-    console.error(dispatchOrHistoryErr)
+    console.error(dispatchOrHistoryErr);
   }
-}
+};
 
 export const logout = () => async dispatch => {
   try {
-    await axios.post('/auth/logout')
-    dispatch(removeUser())
-    history.push('/login')
+    await axios.post("/auth/logout");
+    dispatch(removeUser());
+    history.push("/login");
   } catch (err) {
-    console.error(err)
+    console.error(err);
   }
-}
+};
 
 export default function(state = defaultUser, action) {
   switch (action.type) {
     case GET_USER:
-      return action.user
+      return action.user;
     case REMOVE_USER:
-      return defaultUser
+      return defaultUser;
     default:
-      return state
+      return state;
   }
 }
