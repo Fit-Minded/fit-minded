b6aa0fe3d72560188b7df8e43f84e13334c90a33
Roy-Tessler <23tessler@gmail.com>
2020-02-03 17:16:08 -0500

    added liked me

--- src/store/pool.js
+++ src/store/pool.js
@@ -1,9 +1,8 @@
-<<<<<<< HEAD
-import axios from "axios";
-import history from "../history";
-const GOT_LIKED_ME = "GOT_LIKED_ME";
-const GOT_TO_JUDGE = "GOT_TO_JUDGE";
-const MADE_DECISION = "MADE_DECISION";
+import axios from 'axios';
+import history from '../history';
+const GOT_LIKED_ME = 'GOT_LIKED_ME';
+const GOT_TO_JUDGE = 'GOT_TO_JUDGE';
+const MADE_DECISION = 'MADE_DECISION';
 const defaultPool = {
   toJudge: [],
   usersWhoLikedMe: []
@@ -11,24 +10,10 @@ const defaultPool = {
 const gotLikedMe = users => ({ type: GOT_LIKED_ME, users });
 const gotToJudge = toJudge => ({ type: GOT_TO_JUDGE, toJudge });
 const madeDecision = () => ({ type: MADE_DECISION });
-=======
-import axios from 'axios'
-// import history from "../history";
-
-const GOT_TO_JUDGE = 'GOT_TO_JUDGE'
-const MADE_DECISION = 'MADE_DECISION'
-
-const defaultPool = {
-  toJudge: []
-}
-
-const gotToJudge = toJudge => ({ type: GOT_TO_JUDGE, toJudge })
-const madeDecision = () => ({ type: MADE_DECISION })
->>>>>>> 3470cedde801dc1f7014f21ab9c3d4cbc6411b2a
 
 export const getLikedMe = () => async dispatch => {
   try {
-    const { data } = await axios.get("/api/pool/likedMe");
+    const { data } = await axios.get('/api/pool/likedMe');
     dispatch(gotLikedMe(data));
   } catch (error) {
     console.error(error);
@@ -37,19 +22,19 @@ export const getLikedMe = () => async dispatch => {
 
 export const getToJudge = () => async dispatch => {
   try {
-    const { data } = await axios.get('/api/pool/toJudge')
-    dispatch(gotToJudge(data))
+    const { data } = await axios.get('/api/pool/toJudge');
+    dispatch(gotToJudge(data));
   } catch (error) {
-    console.error(error)
+    console.error(error);
   }
-}
+};
 
 export const makeDecision = (decisionType, otherUserId) => async dispatch => {
   try {
-    await axios.put('/api/decision', { decisionType, otherUserId })
-    dispatch(madeDecision())
+    await axios.put('/api/decision', { decisionType, otherUserId });
+    dispatch(madeDecision());
   } catch (error) {}
-}
+};
 
 export default function(state = defaultPool, action) {
   switch (action.type) {
@@ -57,12 +42,12 @@ export default function(state = defaultPool, action) {
       const usersWhoLikedMe = [...action.users];
       return { ...state, usersWhoLikedMe };
     case GOT_TO_JUDGE:
-      const toJudge = [...action.toJudge]
-      return { ...state, toJudge }
+      const toJudge = [...action.toJudge];
+      return { ...state, toJudge };
     case MADE_DECISION:
-      const toJudgeMinusOne = state.toJudge.slice(1)
-      return { ...state, toJudge: toJudgeMinusOne }
+      const toJudgeMinusOne = state.toJudge.slice(1);
+      return { ...state, toJudge: toJudgeMinusOne };
     default:
-      return state
+      return state;
   }
 }
