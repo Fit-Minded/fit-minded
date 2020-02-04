c77af903cbba671bcfa27a6a8b3de54f8a378132
Manami Ueda <manamiueda1@gmail.com>
2020-02-04 13:02:59 -0500

    chat is working

--- src/Routes.js
+++ src/Routes.js
@@ -1,11 +1,12 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { withRouter, Route, Switch } from 'react-router-dom';
-import { Login, Signup } from './components/AuthForm';
-import { DecisionPage, SignUpPage } from './components';
-import { me } from './store';
-import LikedMe from './components/LikedMe';
-import Matches from './components/Matches';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { withRouter, Route, Switch } from "react-router-dom";
+import { Login, Signup } from "./components/AuthForm";
+import { DecisionPage, SignUpPage } from "./components";
+import { me } from "./store";
+import LikedMe from "./components/LikedMe";
+import Matches from "./components/Matches";
+import ChatApp from "./components/ChatApp";
 
 class Routes extends Component {
   componentDidMount() {
@@ -21,15 +22,16 @@ class Routes extends Component {
       <Switch>
         {isLoggedIn ? (
           <Switch>
-            <Route exact path='/home' component={DecisionPage} />
-            <Route exact path='/likedMe' component={LikedMe} />
-            <Route exact path='/matches' component={Matches} />
+            <Route exact path="/home" component={DecisionPage} />
+            <Route exact path="/likedMe" component={LikedMe} />
+            <Route exact path="/matches" component={Matches} />
           </Switch>
         ) : (
           <Switch>
-            <Route exact path='/login' component={Login} />
-            <Route exact path='/signup' component={Signup} />
-            <Route exact path='/signUpPage' component={SignUpPage} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/signup" component={Signup} />
+            <Route exact path="/signUpPage" component={SignUpPage} />
+            <Route exact path="/chat" component={ChatApp} />
           </Switch>
         )}
       </Switch>
---
+++ src/components/Title.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Title() {
+  return <p class="title">My awesome chat app</p>;
+}
+
+export default Title;
