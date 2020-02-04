dd09df295b235a37c4c31673b48b738cd625b14c
Roy-Tessler <23tessler@gmail.com>
2020-02-04 15:07:31 -0500

    basic matches

--- src/components/Matches.js
+++ src/components/Matches.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { makeDecision, getMatches } from '../store';
+import { Link } from 'react-router-dom';
 
 class Matches extends Component {
   constructor(props) {
@@ -35,25 +36,25 @@ class Matches extends Component {
           {matches.map((user, index) => {
             let activities = Object.keys(user.activities);
             return (
-              <div key={index} className='single-match-container'>
-                <img
-                  className='profile-pic-toJudge'
-                  src={user.image}
-                  alt='user-pic'
-                />
-                <div className='match-info'>
-                  <h1>
-                    {user.firstName} {user.lastName}
-                  </h1>
-                  <h1>
-                    {user.gender.own}, {user.age.own}
-                  </h1>
-                  {activities.map((activity, index) => {
-                    return <h2 key={index}>{activity}</h2>;
-                  })}
+              <Link to='/'>
+                <div key={index} className='single-match-container'>
+                  <img
+                    className='profile-pic-matches'
+                    src={user.image}
+                    alt='user-pic'
+                  />
+                  <div className='match-info'>
+                    <p>
+                      {user.firstName} {user.lastName}
+                    </p>
+
+                    {activities.map((activity, index) => {
+                      return <p key={index}>{activity}</p>;
+                    })}
+                    <i class='fas fa-comment-dots'></i>
+                  </div>
                 </div>
-                <button type='button'>Let's Chat</button>
-              </div>
+              </Link>
             );
           })}
         </div>
