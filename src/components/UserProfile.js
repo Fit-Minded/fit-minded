9b56d4d34a6b6531d63a8f98e1391f05c58bc79f
Adel Jabbar <adeljabb@gmail.com>
2020-02-05 15:36:28 -0500

    minor styling for user profile and decision page

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -9,16 +9,26 @@ class UserProfile extends Component {
 
   render() {
     const { logout } = this.props
-    const { image, firstName, lastName } = this.props.user
+    let { image, firstName, lastName, activities, age, gender } = this.props.user
     console.log(this.props.user)
+    activities = Object.keys(activities)
     return (
       <div id="userProfile">
         <h1>
           {firstName} {lastName}
         </h1>
         <img src={image} alt="userPic" />
+        {activities.map((activity, index) => {
+          return (
+            <div className="activity-cont" key={index}>
+              {activity}
+            </div>
+          )
+        })}
+        <div>Age: {age.own}</div>
+        <div>Gender: {gender.own}</div>
 
-        <button type="button" onClick={logout}>
+        <button className="logout-btn" type="button" onClick={logout}>
           LOGOUT
         </button>
       </div>
