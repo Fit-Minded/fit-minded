9cf0b0fc7a62bb699151d1805dac27da81586eaf
Robert Fox <robert@foxs.com>
2020-02-05 10:42:36 -0500

    login page update

--- src/Routes.js
+++ src/Routes.js
@@ -19,7 +19,6 @@ class Routes extends Component {
 
   render() {
     const { isLoggedIn } = this.props
-    console.log(this.props)
 
     return (
       <Switch>
--- src/components/AuthForm.js
+++ src/components/AuthForm.js
@@ -1,83 +1,78 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
-import { auth } from '../store';
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
+import { auth } from '../store'
 
 class AuthForm extends Component {
   constructor(props) {
-    super(props);
+    super(props)
 
     this.state = {
       email: '',
       password: '""'
-    };
-    this.handleChange = this.handleChange.bind(this);
+    }
+    this.handleChange = this.handleChange.bind(this)
   }
 
   handleChange(e) {
     this.setState({
       ...this.state,
       [e.target.name]: e.target.value
-    });
+    })
   }
 
   render() {
-    const { name, displayName, handleSubmit, error } = this.props;
+    const { name, displayName, handleSubmit, error } = this.props
     return (
-      <div id='authForm'>
-        <form onSubmit={handleSubmit} name={name} className='login-form'>
-          <div className='form-container'>
-            <h1 className='log-or-sign-head'>{name.toUpperCase()}</h1>
-            <div>
-              <input
-                name='email'
-                type='text'
-                className='form-input-box'
-                placeholder='✉️  Email'
-                onChange={this.handleChange}
-              />
-            </div>
-            <br />
-            <div>
-              <input
-                name='password'
-                type='password'
-                className='form-input-box'
-                placeholder='🔒 *******'
-                onChange={this.handleChange}
-              />
-            </div>
-            <div className='remember-chbx'>
-              <input type='checkbox' name='remember-me' className='chbx-btn' />
-              Remember Me
-            </div>
-            <br />
-            <div id='authSubmit'>
-              {displayName === 'Sign Up' ? (
-                <Link
-                  to={{
-                    pathname: '/signUpPage',
-                    state: {
-                      email: this.state.email,
-                      password: this.state.password
-                    }
-                  }}
-                >
-                  <button type='button' className='login-signup-btn'>
-                    SIGN UP
-                  </button>
-                </Link>
-              ) : (
-                <button type='submit' className='login-signup-btn'>
-                  LOG IN
-                </button>
-              )}
-            </div>
-            {error && error.response && <div> {error.response.data} </div>}
+      <div id="authForm">
+        <h1 className="title">
+          Fit<span>Minded</span>
+        </h1>
+        <form onSubmit={handleSubmit} name={name} className="login-form">
+          <h1>{name.toUpperCase()}</h1>
+          <div>
+            <input
+              name="email"
+              type="text"
+              placeholder="✉️  Email"
+              onChange={this.handleChange}
+            />
           </div>
+          <br />
+          <div>
+            <input
+              name="password"
+              type="password"
+              placeholder="🔒 *******"
+              onChange={this.handleChange}
+            />
+          </div>
+          <div className="remember-chbx">
+            <input type="checkbox" name="remember-me" className="chbx-btn" />
+            Remember Me
+          </div>
+          <br />
+          <div id="authSubmit">
+            {displayName === 'Sign Up' ? (
+              <Link
+                to={{
+                  pathname: '/signUpPage',
+                  state: {
+                    email: this.state.email,
+                    password: this.state.password
+                  }
+                }}
+              >
+                <button type="button">SIGN UP</button>
+              </Link>
+            ) : (
+              <button type="submit">LOG IN</button>
+            )}
+          </div>
+          {error && error.response && <div> {error.response.data} </div>}
         </form>
       </div>
-    );
+    )
   }
 }
 
@@ -86,28 +81,28 @@ const mapLogin = state => {
     name: 'login',
     displayName: 'Login',
     error: state.user.error
-  };
-};
+  }
+}
 
 const mapSignup = state => {
   return {
     name: 'signup',
     displayName: 'Sign Up',
     error: state.user.error
-  };
-};
+  }
+}
 
 const mapDispatch = dispatch => {
   return {
     handleSubmit(evt) {
-      evt.preventDefault();
-      const formName = evt.target.name;
-      const email = evt.target.email.value;
-      const password = evt.target.password.value;
-      dispatch(auth({ email, password }, formName));
+      evt.preventDefault()
+      const formName = evt.target.name
+      const email = evt.target.email.value
+      const password = evt.target.password.value
+      dispatch(auth({ email, password }, formName))
     }
-  };
-};
+  }
+}
 
-export const Login = connect(mapLogin, mapDispatch)(AuthForm);
-export const Signup = connect(mapSignup, mapDispatch)(AuthForm);
+export const Login = connect(mapLogin, mapDispatch)(AuthForm)
+export const Signup = connect(mapSignup, mapDispatch)(AuthForm)
