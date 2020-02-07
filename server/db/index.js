0418bff0f149b3aaac3c2c3726069bca9a82b9cc
Robert Fox <robert@foxs.com>
2020-02-07 12:08:39 -0500

    mongo address

--- server/db/index.js
+++ server/db/index.js
@@ -1,7 +1,11 @@
 const mongoose = require('mongoose')
 
+const dbAddress = (process.env.NODE_ENV = 'production'
+  ? process.env.MONGODB_URI
+  : 'mongodb://localhost:27017/fit-minded')
+
 const connect = () => {
-  return mongoose.connect('mongodb://localhost:27017/fit-minded', {
+  return mongoose.connect(dbAddress, {
     useNewUrlParser: true,
     useUnifiedTopology: true
   })
