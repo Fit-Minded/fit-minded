bcebd0e2f2e0d7d5bcad953acf2c74292dc5dae0
Robert Fox <robert@foxs.com>
2020-02-07 13:29:30 -0500

    asd

--- server/db/index.js
+++ server/db/index.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose')
 
-const dbAddress = (process.env.NODE_ENV = 'production'
-  ? process.env.MONGODB_URI
-  : 'mongodb://localhost:27017/fit-minded')
+const dbAddress =
+  process.env.NODE_ENV === 'production'
+    ? process.env.MONGODB_URI
+    : 'mongodb://localhost:27017/fit-minded'
 
 const connect = () => {
   return mongoose.connect(dbAddress, {
